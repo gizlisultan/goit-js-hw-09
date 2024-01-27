fe7e503838c9f9a87a58e31f2f5dc772f393fc07
gizlisultan <seytoplaev9802@gmail.com>
2024-01-27 11:16:32 +0100

    updates

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -12,23 +12,34 @@ form.addEventListener("input", infoInput)
 
 function infoInput(evt) {
     const formInputs = {}
-    formInputs.email = evt.currentTarget.email.value.trim();
-    formInputs.message = evt.currentTarget.message.value.trim();
+    formInputs.email = evt.currentTarget.email.value;
+    formInputs.message = evt.currentTarget.message.value;
     
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formInputs))
 
    
 }
+function getInfo() {
+    const info = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (info) {
+    email.value = info.email;
+    message.value = info.message;
+  }
+}
+getInfo();    
+
 function infoSubmit(evt) {
-    evt.preventDefault()
+    evt.preventDefault();
+ 
     const email = evt.currentTarget.email.value;
     const message = evt.currentTarget.message.value;
   if (email === "" || message === "") {
-    alert('Please fill in all the fields!');
+    alert("Please fill in all the fields!");
     return;
-  }
-     localStorage.removeItem(STORAGE_KEY),
+    }
+    localStorage.removeItem(STORAGE_KEY),
     evt.currentTarget.reset()
+  
 
 }
